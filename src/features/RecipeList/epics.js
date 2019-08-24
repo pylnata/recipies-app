872c6adeb650922c05e2ebe75f07440b3e7a01c8
pylnata <pnataliya@icloud.com>
2019-08-24 18:27:07 +0200

    epics fix

--- src/features/RecipeList/epics.js
+++ src/features/RecipeList/epics.js
@@ -4,36 +4,20 @@ import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
 
-
 import "rxjs/add/observable/of";
 
 import { ofType } from "redux-observable";
 
-
-import {
-  mergeMap,
-  catchError,
-  filter,
-  switchMap,
-  map,
-  takeUntil,
-
-} from "rxjs/operators";
+import { catchError, switchMap, map } from "rxjs/operators";
 
 import * as api from "../../service/api";
 
-import {
-  actionTypes,
-  searchSuccess,
-  searchFail
-} from "./actions";
-
-
+import { actionTypes, searchSuccess, searchFail } from "./actions";
 
 export const searchEpic = action$ =>
   action$.pipe(
     ofType(actionTypes.SEARCH),
-    mergeMap(action =>
+    switchMap(action =>
       from(api.getSearchResults(action.query, action.offset)).pipe(
         map(response => searchSuccess(response.data)),
         catchError(error => {
