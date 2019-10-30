ec4f72d88d862acfc8e114c498a73dfa80538aa2
pylnata <pnataliya@icloud.com>
2019-10-30 14:15:20 +0100

    Updates

---
+++ precache-manifest.599d90cc46c9eac98ef5bb85d5ad7899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167d2f8f1eead2eec49ba26e27f4fa49",
+    "url": "/recipies-app/index.html"
+  },
+  {
+    "revision": "7fbc8cd33aac2b1c30d5",
+    "url": "/recipies-app/static/css/main.3e2f77d6.chunk.css"
+  },
+  {
+    "revision": "0c25ef6369fea46cea89",
+    "url": "/recipies-app/static/js/2.82e65bc0.chunk.js"
+  },
+  {
+    "revision": "7fbc8cd33aac2b1c30d5",
+    "url": "/recipies-app/static/js/main.43937e25.chunk.js"
+  },
+  {
+    "revision": "cd7c7e8e1aad2a06aafa",
+    "url": "/recipies-app/static/js/runtime~main.6b434911.js"
+  },
+  {
+    "revision": "b6abe87aa60ae251e607064fa02ee124",
+    "url": "/recipies-app/static/media/bg.b6abe87a.jpg"
+  }
+]);
